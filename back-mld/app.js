f06973234627253222b3dc6b3f20011ee2e6a201
saad942 <ammarisaad343@gmail.com>
2026-01-17 23:22:27 +0100

    Railway  check

--- back-mld/app.js
+++ back-mld/app.js
@@ -21,10 +21,7 @@ app.use(express.static("uploads"));
 /* =======================
    ROOT + HEALTH (CRITICAL)
 ======================= */
-// Railway MUST get a fast response on "/"
-app.get("/", (req, res) => {
-  res.status(200).send("App is running");
-});
+
 
 app.get("/health", (req, res) => {
   res.status(200).send("OK");
