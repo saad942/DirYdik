4cc47c7c16e577fa05ce678981892700463fc86b
saad942 <ammarisaad343@gmail.com>
2026-01-17 23:40:23 +0100

    end

--- back-mld/app.js
+++ back-mld/app.js
@@ -9,7 +9,7 @@ const app = express();
 /* =======================
    PORT (CRITICAL)
 ======================= */
-const PORT = process.env.PORT || 3000;
+const PORT =3002;
 
 /* =======================
    MIDDLEWARE
